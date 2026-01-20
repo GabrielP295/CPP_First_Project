ca2870d5a99421a8b5d8bcbe7906dbbc8e6a8577
GabrielP295 <gabyboyxp@gmail.com>
2026-01-20 10:16:17 -0600

    Simple payment program implemented

--- MyFirstProject/MyFirstProject.cpp
+++ MyFirstProject/MyFirstProject.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Gabriel Paniagua!\n";
+    int hours = 40;
+    double rate = 52;
+	double payment = hours * rate;
+    double paymentPerMonth = payment * 4;
+	double paymentAcrossInternship = paymentPerMonth * 3;
+    
+
+    cout << "Payment is: $" << payment << endl;
+	cout << "Payment per month is: $" << paymentPerMonth << endl;
+	cout << "Payment across internship is: $" << paymentAcrossInternship << endl;
+    cout << "Gabriel Paniagua!\n";
 }
 
